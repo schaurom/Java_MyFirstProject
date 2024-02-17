2fef50a7cba54671506852effd99daa4bcccf0bf
schaurom <118014904+schaurom@users.noreply.github.com>
2024-02-17 23:52:03 +0100

    .

---
+++ src/Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+
+    public static void main(String[] args){
+        // Dies ist ein Kommentar
+        // Dies ist ebenfalls ein Kommentar
+        // System.out.println("Hello World");
+
+        int x; // Variablendeklaration
+
+        /*
+        * Das ist
+        * ein mehrzeiliger
+        * Kommentar
+        * und noch eine weitere Zeile
+         */
+    }
+}
